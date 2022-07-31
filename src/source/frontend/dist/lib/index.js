fdfaad8d745e650cd0e11255ea8878b0748ede0e
Elijah Bodden <106613755+Elijah-Bodden@users.noreply.github.com>
2022-07-31 11:20:22 -0500

    Force WebSocket to port 8777

--- src/source/frontend/dist/lib/index.js
+++ src/source/frontend/dist/lib/index.js
@@ -234,7 +234,7 @@ var defaultConfig = {
   },
   serverLink: {
     initBindURL: `ws://${window.location.hostname}:8777/bind?originatingSDP=*`,
-    reconnectURL: `ws://${window.location.hostname}/reconnect`,
+    reconnectURL: `ws://${window.location.hostname}:8777/reconnect`,
     reconnectInterval: 5000,
   },
   UI: {
